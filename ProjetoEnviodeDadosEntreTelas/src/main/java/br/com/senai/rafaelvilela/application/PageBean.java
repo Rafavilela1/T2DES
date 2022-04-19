c095337076f12b40c96cdff6605c0325db4a788d
aluno <aluno@DSN1251230715>
2022-04-19 17:01:31 -0300

    commit

---
+++ ProjetoEnviodeDadosEntreTelas/src/main/java/br/com/senai/rafaelvilela/application/PageBean.java
@@ -0,0 +1,35 @@
+package br.com.senai.rafaelvilela.application;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped
+public class PageBean {
+	private String nome;
+
+	@Inject
+	private Flash flash;
+	
+	
+	public String processar() {
+		
+		flash.put("nomeDoUsuario", nome);
+		return "result?faces-redirect=true";
+	}
+
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
