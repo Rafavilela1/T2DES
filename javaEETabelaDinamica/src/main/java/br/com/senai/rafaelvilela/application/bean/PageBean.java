6a40f1b8aab18b67352d12ce4f6b94321cdbdb20
aluno <aluno@DSN1251230715>
2022-05-10 17:13:17 -0300

    commit

---
+++ javaEETabelaDinamica/src/main/java/br/com/senai/rafaelvilela/application/bean/PageBean.java
@@ -0,0 +1,52 @@
+package br.com.senai.rafaelvilela.application.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.model.ListDataModel;
+import javax.inject.Named;
+
+import br.com.senai.rafaelvilela.application.model.Despesas;
+
+@Named("tabela")
+@SessionScoped
+public class PageBean implements Serializable {
+	private List<Despesas> despesasList = new ArrayList<>();
+	private ListDataModel<Despesas> despesas = new ListDataModel<>(despesasList);
+	
+	public String inserir() {
+		Despesas d = new Despesas();
+		d.setEdit(true);
+		despesasList.add(d);
+		
+		
+		return null;
+	}
+	
+	
+	public String excluir(Despesas despesa) {
+		despesasList.remove(despesa);
+		return null;
+	}
+	public String editar(Despesas despesa) {
+		despesa.setEdit(true);
+		return null;
+	}
+	public String gravar(Despesas despesa) {
+		despesa.setEdit(false);
+		return null;
+	}
+	
+	public String listaDataModel(Despesas despesa) {
+		despesa.setEdit(false);
+		return null;
+	}
+	public ListDataModel<Despesas> getDespesas() {
+		
+		return despesas;
+	}
+
+
+}
---
+++ javaEETabelaDinamica/src/main/java/br/com/senai/rafaelvilela/application/model/Despesas.java
@@ -0,0 +1,39 @@
+package br.com.senai.rafaelvilela.application.model;
+
+import java.util.Date;
+
+public class Despesas {
+	private String descricao;
+	private Double valor;
+	private boolean edit;
+	private String data;
+	
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public Boolean getEdit() {
+		return edit;
+	}
+	public void setEdit(boolean edit) {
+		this.edit = edit;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	
+}
